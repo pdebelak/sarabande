e696e68a50c45e10b9dfc85a5528699491aae2b1
pdebelak <pdebelak@gmail.com>
2018-05-11 08:54:32 -0500

    Add escape hatch to add arbitrary html to post or page
    Also added some styling cleanup

--- sarabande/models/cms_model.py
+++ sarabande/models/cms_model.py
@@ -1,6 +1,15 @@
+import re
+
 from bs4 import BeautifulSoup
 from markupsafe import Markup
 from slugify import slugify
+
+
+def _replace_magic_text(text):
+    return re.sub(
+        r'<p>\s*===\s*</p>(.+?)<p>\s*===\s*</p>',
+        lambda match: Markup(match.group(1)).unescape(),
+        text)
 
 
 class CMSModel(object):
@@ -9,7 +18,7 @@ class CMSModel(object):
 
     @property
     def html_body(self):
-        return Markup(self.body)
+        return Markup(_replace_magic_text(self.body))
 
     @property
     def summary(self):
